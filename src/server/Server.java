1b6c68221910b034ec2d9474271edd0838b1bca8
Bintousk <senibintou@gmail.com>
2022-06-02 20:48:55 -0400

    mise en marche du  server

--- src/server/Server.java
+++ src/server/Server.java
@@ -7,35 +7,34 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server
-{
+public class Server {
     private static ServerSocket listener;
+    public static void main(String[] args) throws Exception{
 
-    public static void main(String[] args) throws Exception {
-
-
-        // Someteur incrémenté à shaque connexien d'un client au serveun
+        // Compteur incrémenté à chaque connexien d'un client au serveur
         int clientNumber = 0;
         // Adresse et port du serveun
         String serverAddress = "127.0.0.1";
         int serverPort = 5000;
-// créatian de la sonnexian pour communiquer aves les clients
+        // créatian de la connexian pour communiquer aves les clients
         listener = new ServerSocket();
         listener.setReuseAddress(true);
         InetAddress serverIP = InetAddress.getByName(serverAddress);
-// Association de l'adresse et. du port à la connexien
+        // Association de l'adresse et. du port à la connexien
         listener.bind(new InetSocketAddress(serverIP, serverPort));
-        System.out.format("The server is running on %:%d%n", serverAddress, serverPort);
+        System.out.format("The server is running on %s:%d%n", serverAddress, serverPort);
         try {
             while (true) {
-// Important : la fonstion accept() est blaquante : attend qu'un proshain client se connecte
-// Une nouvetle connection : on incémente le compteur clientNumber
-    new ClientHandler(listener.accept(), clientNumber++).start();
-}
-} finally {
+                // Important : la fonction accept() est bloquante : attend qu'un proshain client se connecte
+                // Une nouvelle connection : on incémente le compteur clientNumber
+                new ClientHandler(listener.accept(), clientNumber++).start();
+            }
+        }
+        finally
+        {
         listener.close();
         }
-}
+    }
     private static class ClientHandler extends Thread {
         private Socket socket;
         private int clientNumber;
@@ -43,25 +42,24 @@ public class Server
         public ClientHandler(Socket socket, int clientNumber) {
             this.socket = socket;
             this.clientNumber = clientNumber;
-            System.out.println("New connection with client#" + clientNumber
-                    + " at " + socket);
+            System.out.println("New connection with client#" + clientNumber + " at " + socket);
         }
 
-        /*
-        s:
-        Une thread se charge d'envoyer au client un message de bienvenue
-        */
-        public void run() {
+
+    //Une thread se charge d'envoyer au client un message de bienvenue
+
+        public void run()
+        {
             try {
-// création d'un canal sontant pour envover des messages au client
+                // création d'un canal sortant pour envoyer des messages au client
                 DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-// Envoie d'un message au client
+                // Envoie d'un message au client
                 out.writeUTF("Hello from server you are client#" + clientNumber);
             } catch (IOException e) {
-                System.out.println("Error handling client#" + clientNumber + ": " + e);
+                System.out.println("Error handling client#" + clientNumber + " : " + e);
             } finally {
                 try {
-// fermetuce de la sonnexien aves le client
+                // fermetuce de la sonnexien aves le client
                     socket.close();
                 }
                 catch(IOException e){
