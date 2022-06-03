8c5b54f3dc084fca2323a95280c3a2365475aafa
Mathias Gagnon <mathias.gagnon@polymtl.ca>
2022-06-03 14:31:48 -0400

    server demande et valide l'addresse

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,33 +1,54 @@
 package server;
 
+import Client.UserInputGetter;
+
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.*;
+import java.util.Scanner;
 
 public class Server {
     private static ServerSocket listener;
+    private static Path serverRoot;
+
+
     public static void main(String[] args) throws Exception{
+        Scanner userInput = new Scanner(System.in);
+
+        Path rootDirectory = Paths.get("", "/root");
+        if (Files.notExists(rootDirectory)) {
+            Files.createDirectory(rootDirectory);
+        }
+        serverRoot = Paths.get("root");
+
 
         // Compteur incrémenté à chaque connexien d'un client au serveur
         int clientNumber = 0;
-        // Adresse et port du serveun
-        String serverAddress = "127.0.0.1";
-        int serverPort = 5000;
-        // créatian de la connexian pour communiquer aves les clients
-        listener = new ServerSocket();
-        listener.setReuseAddress(true);
-        InetAddress serverIP = InetAddress.getByName(serverAddress);
-        // Association de l'adresse et. du port à la connexien
-        listener.bind(new InetSocketAddress(serverIP, serverPort));
-        System.out.format("The server is running on %s:%d%n", serverAddress, serverPort);
+
+        String input = UserInputGetter.getInitialInput(userInput);
+
+        {
+            String serverAddress = input.split("!")[0];
+            int serverPort = Integer.parseInt(input.split("!")[1]);
+
+            listener = new ServerSocket();
+            listener.setReuseAddress(true);
+            InetAddress serverIP = InetAddress.getByName(serverAddress);
+            // Association de l'adresse et. du port à la connexien
+            listener.bind(new InetSocketAddress(serverIP, serverPort));
+            System.out.format("The server is running on %s:%d%n", serverAddress, serverPort);
+        }
+
+
         try {
             while (true) {
-                // Important : la fonction accept() est bloquante : attend qu'un proshain client se connecte
+                // Important : la fonction accept() est bloquante : attend qu'un prochain client se connecte
                 // Une nouvelle connection : on incémente le compteur clientNumber
-                new ClientHandler(listener.accept(), clientNumber++).start();
+                new ClientHandler(listener.accept(), clientNumber++, serverRoot).start();
             }
         }
         finally
@@ -38,15 +59,78 @@ public class Server {
     private static class ClientHandler extends Thread {
         private Socket socket;
         private int clientNumber;
+        private Path currentDirectory;
 
-        public ClientHandler(Socket socket, int clientNumber) {
+        public ClientHandler(Socket socket, int clientNumber, Path rootDirectory) {
             this.socket = socket;
             this.clientNumber = clientNumber;
+            currentDirectory = rootDirectory;
             System.out.println("New connection with client#" + clientNumber + " at " + socket);
         }
 
 
     //Une thread se charge d'envoyer au client un message de bienvenue
+
+        private void mkdir(String name) {
+            String answer = "";
+            try {
+                name = "/" + name;
+                Path creationPath = Paths.get(currentDirectory.toString(), name);
+                Files.createDirectory(creationPath);
+            } catch (FileAlreadyExistsException e) {
+                answer = answer + "Error: directory already exists";
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                sendAnswer(answer);
+            }
+        }
+
+        private void cd(String name) {
+            String answer = "";
+            if (name == "..") {
+                if(!currentDirectory.endsWith("root")){
+                    currentDirectory = currentDirectory.getParent();
+                    answer = answer + "it worked";
+                }
+                else {
+                    answer = answer + "Error: already at root directory.";
+                }
+            }
+            else {
+                try {
+                    currentDirectory = currentDirectory.resolve(name);
+                    answer = answer + "Currently on directory: " + name;
+                } catch (InvalidPathException e) {
+                    answer = answer + "Error: Could not resolve path";
+                }
+                finally {
+
+                }
+            }
+            sendAnswer(answer);
+        }
+
+        private void sendAnswer(String answer){
+            try {
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+                out.writeUTF(answer);
+            } catch (IOException e) {
+                System.out.println("Error handling client#" + clientNumber + " : " + e);
+            } finally {
+                try {
+                    socket.close();
+                }
+                catch(IOException e){
+                    System.out.println("Couldn't close a socket, what's going on?");
+                }
+                System.out.println("Connection with client# " + clientNumber + "closed");
+            }
+        }
+
+
 
         public void run()
         {
@@ -69,4 +153,6 @@ public class Server {
             }
         }
     }
+
 }
+
---
+++ src/server/UserInputGetter.java
@@ -0,0 +1,96 @@
+package server;
+
+import java.util.Scanner;
+
+public class UserInputGetter {
+    private Scanner userInput = new Scanner(System.in);
+
+    public static String getInitialInput(Scanner input){
+            String userInput1;
+            do {
+                System.out.print("Entrez l'addresse IP du serveur");
+                userInput1 = input.nextLine();
+            } while (!validateIPAddress(userInput1));
+
+            String userInput2;
+            do {
+                System.out.print("Entrez le port d'écoute du serveur");
+                userInput2 = input.nextLine();
+            } while(!validatePort(userInput2));
+
+            return userInput1 + "!" + userInput2;
+        }
+
+        private static boolean validateIPAddress(String input){
+            String[] parts = input.split("\\.");
+            boolean valide = false;
+
+            if (parts.length == 4){
+                for( String part : parts){
+                    if (!( valide = isNumeral(part)) ) {
+                        break;
+                    }
+                    int value = Integer.parseInt(part);
+                    if (value < 0 || value > 255) {
+                        valide = false;
+                    }
+                }
+            }
+
+            if (!valide){
+                System.out.print("Format d'addresse IP invalide\n");
+            }
+
+            return valide;
+        }
+
+        private static boolean validatePort(String string){
+            boolean valide;
+
+            if (!(valide = isNumeral(string))){
+                System.out.print("Format de port invalide: pas un nombre \n");
+                return false;
+            }
+
+            int port = Integer.parseInt(string);
+            valide = (port >=5000 && port <= 5050);
+
+            if (!valide){
+                System.out.print("Port invalide: Ports disponibles seulement de 5000 a 5050\n");
+            }
+            return valide;
+        }
+
+        private static boolean isNumeral(String string){
+            if (string == null || string .equals("")){
+                return false;
+            }
+
+            try {
+                int intValue = Integer.parseInt(string);
+                return true;
+            } catch(NumberFormatException e) { }
+            return false;
+        }
+
+        static boolean validateCommand(String string){
+            for (String command : commandsRegex){
+                if (string.matches(command)){
+                    return true;
+                }
+            }
+            System.out.print("Commande invalide");
+            return false;
+        }
+
+        static String[] commandsRegex = {
+                "^ls$",
+                "^exit$",
+                "^cd\s.+",
+                "^mkdir\s.+",
+                "^upload\s.+",
+                "^download\s.+"
+        };
+
+
+}
